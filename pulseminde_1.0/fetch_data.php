8ffad4553cf5530505999d3af31836aa7a1adb85
Thisen Nambukara <thisennambukara@gmail.com>
2024-09-08 23:07:51 +0530

    Update fetch_data.php
    2024.9.8

--- pulseminde_1.0/fetch_data.php
+++ pulseminde_1.0/fetch_data.php
@@ -12,7 +12,7 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-$sql = "SELECT temp, heartrate, time FROM esp32 ORDER BY testid DESC LIMIT 10";
+$sql = "SELECT temp, heartrate,stressindex, time FROM esp32 ORDER BY testid DESC LIMIT 10";
 $result = $connection->query($sql);
 
 if (!$result) {
