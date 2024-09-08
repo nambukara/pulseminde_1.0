642bb5e923ee4b75bc2d9cf02ffcb83d49b0a1bb
Thisen Nambukara <thisennambukara@gmail.com>
2024-09-08 22:01:31 +0530

    Add files via upload

---
+++ pulseminde_1.0/fetch_data.php
@@ -0,0 +1,31 @@
+<?php 
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "pulsemind";
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = "SELECT temp, heartrate, time FROM esp32 ORDER BY testid DESC LIMIT 10";
+$result = $connection->query($sql);
+
+if (!$result) {
+    die("Invalid query: " . $connection->error);
+}
+
+$data = array();
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+
+$connection->close();
+?>
